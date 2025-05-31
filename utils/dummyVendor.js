b79d8adcf8e9fba095a611acefcacf679b92fce1
Amit Prajapati <115357958+AmitPrajapati-1@users.noreply.github.com>
2025-05-31 08:53:40 +0530

    Update dummyVendor.js

--- utils/dummyVendor.js
+++ utils/dummyVendor.js
@@ -3,7 +3,7 @@ const campaignModel = require('../models/Campaign');
 async function sendMessage(logId, customer, message) {
   const isSuccess = Math.random() < 0.9;
   console.log(`Sending message to ${customer.name}: ${message} (Log ID: ${logId})`);
-  await axios.post('http://localhost:4000/api/delivery-receipt', {
+  await axios.post('https://crm-backend-production-a717.up.railway.app/api/delivery-receipt', {
     logId,
     status: isSuccess ? 'SENT' : 'FAILED',
   });
