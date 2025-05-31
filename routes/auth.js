e8e7cff6b83f42c37b06e3e92d678b7779dbd633
Amit Prajapati <115357958+AmitPrajapati-1@users.noreply.github.com>
2025-05-31 12:46:57 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post('/login', async (req, res) => {
 });
 
 // Google OAuth
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'],prompt: 'select_account', }));
 
 router.get('/google/callback', passport.authenticate('google', {
   session: false,
